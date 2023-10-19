b933d879c2cd73567a51a2d2bbf9d9d56c2c9be9
Popov Ilya <iipopov_1@edu.hse.ru>
2023-10-19 13:09:48 +0300

    Added week05

---
+++ week05/sem/demo1_kafka/consumer.py
@@ -0,0 +1,26 @@
+import json
+from kafka import KafkaConsumer
+
+KAFKA_HOST = "localhost"
+KAFKA_TOPIC_NAME = "service_backend_feed"
+KAFKA_CONSUMER_GROUP = "backend"
+
+consumer = KafkaConsumer(
+    bootstrap_servers=f"{KAFKA_HOST}:9092",
+    value_deserializer=lambda v: json.loads( v.decode("utf-8") ),
+    auto_offset_reset="earliest",
+    group_id=KAFKA_CONSUMER_GROUP
+)
+consumer.subscribe(topics=KAFKA_TOPIC_NAME)
+
+try:
+    for message in consumer:
+        value = message.value
+        print(value)
+except Exception as e:
+        print("Closing consumer due to error")
+        consumer.close()
+        raise e
+finally:
+    print("Closing consumer due to finish")
+    consumer.close()
